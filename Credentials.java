78cf6174b67e3ee690f4a762f06285fcdaf9c4a3
yhailu <yhailu@hotmail.com>
2018-11-19 12:14:06 -0500

    Update Credentials.java

--- Credentials.java
+++ Credentials.java
@@ -1,7 +1,3 @@
 public interface Credentials {
-    String SETTLE_SERVER = "vfq2-smbsettle-1.ftscc.net";
-    String SETTLE_USER = "yhailu";
-    String SETTLE_PASSWORD = "";
-    String QA2_DB = "jdbc:mysql://vfq-db-1.ftscc.net/qa2";
-    String QA2_DB_PASSWORD = "";
+
 }
